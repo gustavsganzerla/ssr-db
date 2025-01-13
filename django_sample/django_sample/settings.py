e75db550302eac096160882ec6b73221d093d277
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2025-01-13 10:19:14 -0400

    added figures in statistics

--- django_sample/django_sample/settings.py
+++ django_sample/django_sample/settings.py
@@ -117,7 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -127,4 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+
+
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 100000
+
